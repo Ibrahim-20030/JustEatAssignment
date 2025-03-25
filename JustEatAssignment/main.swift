549f69fb66b3045295dc4e94568c8eeeaffb6753
Ibrahim Khizer <ibrahim.khizer@gmail.com>
2025-03-25 17:30:39 +0000

    Get postcode from user and check it's not empty

---
+++ JustEatAssignment/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  JustEatAssignment
+//
+//  Created by Ibrahim Khizer on 25/03/2025.
+//
+
+import Foundation
+
+// 1. Ask the user to enter a UK postcode
+print("Enter your postcode")
+// 2. Read their input from the console
+if let postcode = readLine(), !postcode.isEmpty{
+    print("you entered \(postcode)")
+}else{
+    print("you didn't enter a postcode")
+}
+
+
+
+
+
