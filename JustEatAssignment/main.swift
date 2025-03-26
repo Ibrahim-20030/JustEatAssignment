f36203d54cbf1f3fcbad78d5798d376b941bac0e
Ibrahim Khizer <ibrahim.khizer@gmail.com>
2025-03-26 16:39:25 +0000

    Build Just Eat API URL from postcode input

--- JustEatAssignment/main.swift
+++ JustEatAssignment/main.swift
@@ -12,6 +12,12 @@ print("Enter your postcode")
 // 2. Read their input from the console
 if let postcode = readLine(), !postcode.isEmpty{
     print("you entered \(postcode)")
+    let urlString = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/\(postcode)"
+    guard let url = URL(string: urlString) else {
+        print("Invalid URL")
+        exit(1)
+    }
+    print("URL is valid \(url)")
 }else{
     print("you didn't enter a postcode")
 }
